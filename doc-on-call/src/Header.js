4e3f874e2c3e09bf5268d726582d8b7acf941787
aatman151 <aatman151@gmail.com>
2020-02-15 22:41:39 -0500

    header update
    updated header

--- doc-on-call/src/Header.js
+++ doc-on-call/src/Header.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import './header.css'
+
+
+const headerStyle = {
+  textAlign: 'center',
+  padding: '10px',
+  fontFamily: 'Comic-Sans',
+  fontSize: "24px",
+  display: 'flex',
+  flexDirection: 'row', 
+} 
+
 export default function Header() {
   return (
-    <header style = {headerStyle}>
-      <h1>Doc on Call</h1>
+    <header className='m'>
+      <div className="left_image"><img id="blank" src="./images/White_Square"></img></div>
+      <div><font color="white"><h1><b>Doc on Call</b></h1></font></div>
+      <div className="right_image"><img id="logo" src="#"></img></div>
     </header>
   )
 }
 
-const headerStyle = {
-    textAlign: 'center',
-    padding: '10px',
-    fontFamily: 'Comic-Sans',
-    fontSize: "22px",
-  }
